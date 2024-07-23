d2f8fe4e1662e13e9f5faadbd5fa6a07085feb3d
Pierre-Elie Fauché <github@pierre-elie.me>
2024-07-23 10:33:50 -0400

    ➕ prettier, lodash

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@atlaskit/design-system/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
